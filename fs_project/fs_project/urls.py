abc17e5e24ade104deb1f88bd15a7403e840396e
Shawn <shawnbluce@gmail.com>
2024-05-14 23:05:19 +0800

    feature: add JWT login

--- fs_project/fs_project/urls.py
+++ fs_project/fs_project/urls.py
@@ -15,7 +15,35 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FS API",
+        default_version='v1',
+        description="An api for fs",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path(r'api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(r'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(r'^api/token/verify/$', TokenVerifyView.as_view(), name='token_verify'),
+
+    # swagger config
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
